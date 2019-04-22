b7cba32e02a9dfaae72295b060e16be6bdd7a86a
xkored <xkored@gmail.com>
2019-04-22 17:38:39 +0400

    start

---
+++ app/js/scripts.js
@@ -0,0 +1,56 @@
+$(function() {
+	// $('.team__cnt').owlCarousel({
+ //      nav: false,
+ //      dots: true,
+ //      items: 5,
+	
+ //       navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+ //      loop: true,
+ //      margin: 20,
+ //      center:true,
+ //      autoWidth:true,
+ //      mouseDrag: false,
+ //      responsive : {
+ //        0   : {
+ //            items: 1
+ //        },
+ //        380 : {
+ //            items: 1
+ //        },
+ //        480 : {
+ //            items: 1
+ //        },
+ //        768 : {
+ //            items: 3
+ //        },
+ //        1040 : {
+ //            items: 4
+ //        }
+ //      }
+	// });
+  // tabs 
+  // $(document).ready(function () {
+  //   $(".tabs-content__item:not(:first-child)").hide();
+  //   $(".tabs__container div.tabs__content-item.active-tab").show();
+  //   $('ul.tabs__list > li').click(function () {
+  //     if (!($(this).hasClass('active'))) {
+  //       var thisLi = $(this);
+  //       var numLi = thisLi.index();
+  //       thisLi.addClass('active').siblings().removeClass('active');
+  //       thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
+  //     }
+  //   });
+  // });
+  //Закрываем AjaxForm popup после успешной отправки
+  // $(document).on('af_complete', function(event,res) {
+  //   if(res.success) parent.$.fancybox.close();
+  // });
+  //mobile menu
+  // if ( $(window).width() < 992 ) {
+  //   $(".nav__hamburger").on('click',function() {
+  //     $(this).toggleClass("active");
+  //     $('.nav__menu').toggleClass('active');
+  //     $('body').toggleClass("fixed");
+  //   });
+  // }
+});
